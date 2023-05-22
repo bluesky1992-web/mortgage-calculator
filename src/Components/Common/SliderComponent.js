513e0686be12777738095b056d6dcb6bad8e02df
Ali ibrahim <ali.ibrahim.jasim@gmail.com>
2023-05-22 20:49:02 +0300

    slider component created added props

--- src/Components/Common/SliderComponent.js
+++ src/Components/Common/SliderComponent.js
@@ -15,11 +15,31 @@ export default function SliderComponent({
 }) {
   return (
     <Stack my={1.4}>
-         <Typography variant='subtitle2' >{label}</Typography>
-         <Typography variant='h5'>
-            {unit} {amount} 
-
-         </Typography>
+      <Stack gap={1}>
+        <Typography variant="subtitle2">{label}</Typography>
+        <Typography variant="h5">
+          {unit} {amount}
+        </Typography>
+      </Stack>
+      <Slider
+        min={min}
+        max={max}
+        defaultValue={defaultValue}
+        aria-label="default"
+        valueLabelDisplay="auto"
+        onChange={onChange}
+        value={value}
+        marks
+        step={steps}
+      />
+      <Stack direction="row" justifyContent="space-between">
+        <Typography variant="caption" color="text.secondary">
+          {unit} {min}
+        </Typography>
+        <Typography variant="caption" color="text.secondary">
+          {unit} {max}
+        </Typography>
+      </Stack>
     </Stack>
-  )
+  );
 }
